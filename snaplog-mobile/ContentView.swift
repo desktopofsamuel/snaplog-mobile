06047acc4f422153c73ff621c518649029597f22
Samuel Wong <35249407+desktopofsamuel@users.noreply.github.com>
2023-04-06 23:28:32 +0800

    - add content view - add film model - add AddFilmView

--- snaplog-mobile/ContentView.swift
+++ snaplog-mobile/ContentView.swift
@@ -6,83 +6,119 @@
 //
 
 import SwiftUI
-import CoreData
+
+struct Film: Identifiable {
+    let id = UUID()
+    let name: String
+    let purchaseLocation: String
+    let purchasePrice: Double
+    let date: Date
+}
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
+    @State private var films = [
+        Film(name: "Film 1", purchaseLocation: "Location 1", purchasePrice: 12.99, date: Date()),
+        Film(name: "Film 2", purchaseLocation: "Location 2", purchasePrice: 11.99, date: Date()),
+        Film(name: "Film 3", purchaseLocation: "Location 3", purchasePrice: 8.99, date: Date())
+    ]
+    @State private var showingAddFilmModal = false
+    
+    var body: some View {
+        NavigationView {
+            List(films) { film in
+                VStack(alignment: .leading) {
+                    Text(film.name)
+                    Text(film.purchaseLocation)
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                    Text("Date: \(formattedDate(date: film.date))")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                }
+            }
+            .navigationBarTitle("Snaplog")
+            .navigationBarItems(
+                leading: Button(action: {
+                    // Action to show settings view
+                }) {
+                    Image(systemName: "gear")
+                },
+                trailing: Button(action: {
+                    showingAddFilmModal = true
+                }) {
+                    Image(systemName: "plus")
+                }
+            )
+        }
+        .sheet(isPresented: $showingAddFilmModal) {
+            // Modal to add film object data
+            AddFilmView(films: $films, showingModal: $showingAddFilmModal)
+        }
+        
+    }
+    
+    // Helper function to format date
+       private func formattedDate(date: Date) -> String {
+           let dateFormatter = DateFormatter()
+           dateFormatter.dateFormat = "MMM d, yyyy"
+           return dateFormatter.string(from: date)
+       }
+}
 
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
+struct AddFilmView: View {
+    @Binding var films: [Film]
+    @Binding var showingModal: Bool
+    @State private var name = ""
+    @State private var purchaseLocation = ""
+    @State private var date = Date()
+    @State private var purchasePrice = ""
 
     var body: some View {
         NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
+            Form {
+                Section(header: Text("Film Details")) {
+                    TextField("Name", text: $name)
+                    TextField("Purchase Location", text: $purchaseLocation)
+                    DatePicker("Date", selection: $date, displayedComponents: .date)
+                    TextField("Purchase Price", text: $purchasePrice)
+                        .keyboardType(.decimalPad)
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
+
+                Section {
+                    Button("Save", action: saveFilm)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
                 }
             }
-            Text("Select an item")
+            .navigationBarTitle("Add Film")
+            .navigationBarItems(leading: Button(action: {
+                showingModal = false
+            }, label: {
+                Text("Cancel")
+            }), trailing: Button(action: {
+                saveFilm()
+            }, label: {
+                Text("Save") // Updated to "Save"
+            }))
         }
     }
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+    private func saveFilm() {
+        guard let purchasePrice = Double(purchasePrice) else {
+            return
         }
-    }
 
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+        let film = Film(name: name, purchaseLocation: purchaseLocation, purchasePrice: purchasePrice, date: date)
+        films.append(film)
+        showingModal = false
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
-struct ContentView_Previews: PreviewProvider {
+struct Previews_ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
